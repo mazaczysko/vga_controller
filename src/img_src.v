0c5d870e741b89dcba38f8e81ef5e76a340d480f
mazaczysko <miloszrygula@interia.pl>
2022-10-11 23:22:34 +0200

    Minor changes

--- src/img_src.v
+++ src/img_src.v
@@ -12,34 +12,34 @@ module img_src (
 always @(posedge CLK)
 	if (CE)
 		if (PIX < 128) begin
-			R = 3'd0; 
-			G = 3'd0;
-			B = 2'd0;
+			R <= 3'd0; 
+			G <= 3'd0;
+			B <= 2'd0;
 		end
 		else if (PIX < 256) begin
-			R = 3'd7; 
-			G = 3'd0;
-			B = 2'd0;
+			R <= 3'd7; 
+			G <= 3'd0;
+			B <= 2'd0;
 		end
 		else if (PIX < 384) begin
-			R = 3'd0; 
-			G = 3'd7;
-			B = 2'd0;
+			R <= 3'd0; 
+			G <= 3'd7;
+			B <= 2'd0;
 		end
 		else if (PIX < 512) begin
-			R = 3'd0; 
-			G = 3'd0;
-			B = 2'd3;
+			R <= 3'd0; 
+			G <= 3'd0;
+			B <= 2'd3;
 		end
 		else if (PIX < 640) begin
-			R = 3'd7; 
-			G = 3'd7;
-			B = 2'd3;
+			R <= 3'd7; 
+			G <= 3'd7;
+			B <= 2'd3;
 		end
 		else begin
-			R = 3'd0; 
-			G = 3'd0;
-			B = 2'd0;
+			R <= 3'd0; 
+			G <= 3'd0;
+			B <= 2'd0;
 		end
 
 
